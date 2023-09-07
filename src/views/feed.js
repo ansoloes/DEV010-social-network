5e373888d531502d4b6af79f8408d523f0916cda
CaroMV <carolina.moreno.v@ug.uchile.cl>
2023-09-07 14:50:36 -0300

    Caro: Todas las vistas por ahora, más unos provisionales

---
+++ src/views/feed.js
@@ -0,0 +1,11 @@
+function feed(){
+    const title = document.createElement('h2');
+    title.textContent = 'Acá debería ir el Feed';
+    const imaginacion = document.createElement('img');
+    imaginacion.src = "https://www.latercera.com/resizer/wvCMr6B4HSnbYmlIGtaDQVfuch4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/OI7WIZAY6NDH7AQHHZ2NLXTCX4.jpg";
+    imaginacion.alt = "bobesponja";
+    // al body
+    document.body.appendChild(title);
+    document.body.appendChild(imaginacion);
+}
+export default feed;
