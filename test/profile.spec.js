1283be900b29f7cbd512bd3da7b550dd4775b21b
Andrea Garcia <anddysumptermx@gmail.com>
2023-10-09 16:46:01 -0600

    andy: principalregister.spec.js pasando todos los test

--- test/profile.spec.js
+++ test/profile.spec.js
@@ -1,21 +1,26 @@
-/**
- * @jest-environment jsdom
- */
 import { signOut } from 'firebase/auth';
 import profile from '../src/views/profile';
 import { getPosts } from '../src/lib/index.js';
 import { auth } from '../src/lib/firebaseConfig.js';
 
+// Mock para la función getPosts
+jest.mock('../src/lib/index.js', () => ({
+  getPosts: jest.fn(),
+}));
+
 describe('profile', () => {
   let navigateToMock;
 
   beforeEach(() => {
+    // Establecer mocks y configuración inicial
+    jest.clearAllMocks();
     navigateToMock = jest.fn();
     document.body.innerHTML = `
       <div class="profile-image"></div>
       <p class="welcome-message"></p>
     `;
   });
+
   afterEach(() => {
     document.body.innerHTML = '';
   });
@@ -23,9 +28,12 @@ describe('profile', () => {
   it('debería llamar a navigateTo al hacer clic en el botón "Logout"', async () => {
     const mainElement = profile(navigateToMock);
     document.body.appendChild(mainElement);
+
+    // Simular clic en el botón de logout
     const logoutButton = document.querySelector('.logout-button');
     logoutButton.click();
 
+    // Verificar las interacciones esperadas
     expect(signOut).toHaveBeenCalled();
     expect(navigateToMock).toHaveBeenCalledWith('/login');
   });
@@ -35,16 +43,17 @@ describe('profile', () => {
       { title: 'Post 1', content: 'Contenido del post 1', userId: 'user123' },
       { title: 'Post 2', content: 'Contenido del post 2', userId: 'user123' },
     ];
-    jest.spyOn(window, 'getPosts').mockImplementation((callback) => {
-      callback(mockPosts);
-    });
+
+    // Simular la llamada a getPosts y proporcionar el mock de posts
+    getPosts.mockResolvedValue(mockPosts);
 
     const mainElement = profile(navigateToMock);
     document.body.appendChild(mainElement);
 
-    expect(getPosts).toHaveBeenCalled();
+    // Esperar a que se resuelva la promesa de getPosts
+    await Promise.resolve();
 
-    // * check que se mostraron los posts en el DOM
+    // Verificar que se mostraron los posts en el DOM
     const postingArea = document.querySelector('.posting-area');
     expect(postingArea.children).toHaveLength(mockPosts.length);
   });
@@ -52,9 +61,11 @@ describe('profile', () => {
   it('debería mostrar el perfil del usuario correctamente', () => {
     const mainElement = profile(navigateToMock);
     document.body.appendChild(mainElement);
+
     const profileImage = document.querySelector('.profile-image');
     const welcomeMessage = document.querySelector('.welcome-message');
 
+    // Verificar que se muestra correctamente el perfil
     expect(profileImage).toBeTruthy();
     expect(welcomeMessage.textContent).toContain(`Perfil de ${auth.currentUser.displayName}`);
   });
@@ -62,9 +73,12 @@ describe('profile', () => {
   it('debería llamar a navigateTo al hacer click en el botón "Home" en el navbar', () => {
     const mainElement = profile(navigateToMock);
     document.body.appendChild(mainElement);
+
+    // Simular clic en el botón "Home"
     const homeButton = document.querySelector('.footer-home');
     homeButton.click();
 
+    // Verificar la interacción esperada
     expect(navigateToMock).toHaveBeenCalledWith('/feed');
   });
 });
